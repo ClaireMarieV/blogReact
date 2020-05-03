c4ce18da36c72d3fb3c226b9880baf74ab63cdab
Claire Vaney <clairevan.54.cv@gmail.com>
2020-05-03 18:00:55 +0200

    debut article dr martens

--- data/shoes/desir.js
+++ data/shoes/desir.js
@@ -5,6 +5,21 @@ export default {
   picture: "/images/shoes/doc.jpg",
   content: (
     <>
+      <p>
+        Ces Dr... Le romantisme grunge de ces boots qui est susciter grâce aux
+        broderies. Ces roses noires adoucissent les Dr.Martens tout en les
+        mettant en valeur. Un ton sur ton relevé par une lumière différente
+        entre le cuir lisse et l'effet mat du fil. Vous vous rappelez sans doute
+        du style jean acid des années 90', où le tartan, les carreaux, les jeans
+        déchirées et les look négligés controlés avaient le vent en poupe. Cette
+        tendance qui a prit son envol il me semble grâce à Nirvana ou Alice in
+        Chains. Ces boots me le rappelent; une nostalgie qui est nostalgie pour
+        les style vestimentaires et musicaux de l'époque. J'aime cette
+        modernisation des tendances aujourd'hui, dans la mode en tout cas!
+      </p>
+      <p>
+      Elles permettent ce retour des années 90 avec plus d'assurance. 
+      </p>
     </>
   ),
 };
