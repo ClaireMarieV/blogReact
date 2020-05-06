ea4df06c76381ec874590f952be3409c2a2498f2
Claire Vaney <clairevan.54.cv@gmail.com>
2020-05-06 13:47:14 +0200

    creation de la page 'à propos' et deplacement de certains articles dans la page style

--- data/articleStyle.js
+++ data/articleStyle.js
@@ -1,25 +1,31 @@
 import acceptation from "./style/acceptation";
 import acid from "./style/acid";
-import ombre from './style/ombre'
+import ombre from "./style/ombre";
 export default [
   acceptation,
   acid,
   {
     category: "STYLE",
+    title: "UNE AQUARELLE DE FLEURS",
+    date: "09.04.2020",
+    picture: "/images/style/aquarelle.jpg",
+  },
+  {
+    category: "STYLE",
     title: "DU DENIM",
-    date: "06.04.2019",
+    date: "30.03.2019",
     picture: "/images/style/denim1.jpg",
   },
   {
     category: "STYLE",
     title: "IMPRIMÉ VOLANTÉ",
-    date: "06.04.2019",
+    date: "22.04.2019",
     picture: "/images/style/tKooples.png",
   },
   {
     category: "STYLE",
     title: "BOHEME",
-    date: "06.04.2019",
+    date: "17.10.2018",
     picture: "/images/style/photoInsta.jpg",
   },
   {
@@ -30,27 +36,21 @@ export default [
   },
   {
     category: "STYLE",
+    title: "UNE AQUARELLE DE FLEURS",
+    date: "09.04.2020",
+    picture: "/images/style/aquarelle.jpg",
+  },
+  {
+    category: "STYLE",
+    title: "UNE AQUARELLE DE FLEURS",
+    date: "09.04.2020",
+    picture: "/images/style/aquarelle.jpg",
+  },
+  {
+    category: "STYLE",
     title: "DU DENIM",
     date: "06.04.2019",
     picture: "/images/style/denim1.jpg",
-  },
-  {
-    category: "STYLE",
-    title: "UNE AQUARELLE DE FLEURS",
-    date: "09.04.2020",
-    picture: "/images/style/aquarelle.jpg",
-  },
-  {
-    category: "STYLE",
-    title: "UNE AQUARELLE DE FLEURS",
-    date: "09.04.2020",
-    picture: "/images/style/aquarelle.jpg",
-  },
-  {
-    category: "STYLE",
-    title: "UNE AQUARELLE DE FLEURS",
-    date: "09.04.2020",
-    picture: "/images/style/aquarelle.jpg",
   },
   ombre,
 
