0b824e12ede4ccee1ae19195e753bd4f5a65ebd3
Claire Vaney <clairevan.54.cv@gmail.com>
2020-05-18 14:14:53 +0200

    added responsive to card.js

--- components/card.js
+++ components/card.js
@@ -14,6 +14,29 @@ const Card = ({ category, title, children, date, picture, className, id }) => (
         place-content: center;
         grid-gap: 1rem;
       }
+
+      @media (min-width: 800px) and (max-width: 1000px) {
+        .card {
+          grid-template-columns: repeat(3, minmax(0, 1fr));
+        }
+      }
+
+      @media (min-width: 461px) and (max-width: 799px) {
+        .card {
+          grid-template-columns: repeat(2, minmax(0, 1fr));
+        }
+        .grid > .article-splash {
+          margin-bottom: 1em;
+        }
+      }
+      @media (max-width: 460px) {
+        .card {
+          grid-template-columns: repeat(1, minmax(0, 1fr));
+        }
+        .card > .article-splash {
+          margin-bottom: 1em;
+        }
+      }
     `}</style>
   </div>
 );
