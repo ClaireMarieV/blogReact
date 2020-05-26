2acb3b54d646c7d9382687fe08ae0e859da40ffc
Claire Vaney <clairevan.54.cv@gmail.com>
2020-05-26 19:58:51 +0200

    added article basique

--- data/articleShoes.js
+++ data/articleShoes.js
@@ -1,21 +1,5 @@
 import desir from "./shoes/desir";
 import plateforme from "./shoes/plateforme";
 import mango from "./shoes/mangoBottega";
-
-export default [
-  mango,
-  plateforme,
-  {
-    category: "SHOES",
-    title: "SANDALES BOHEME",
-    date: "06.04.2019",
-    picture: "/images/shoes/boheme.jpg",
-  },
-  desir,
-  {
-    category: "SHOES",
-    title: "UN BASIQUE",
-    date: "06.04.2019",
-    picture: "/images/shoes/Studio_2.jpg",
-  },
-];
+import basique from "./shoes/basique";
+export default [mango, plateforme, desir, basique];
--- data/articleStyle.js
+++ data/articleStyle.js
@@ -5,22 +5,4 @@ import vesteAcid from "./style/vesteAcid";
 import aquarelle from "./style/aquarelle";
 import romantisme from "./style/romantisme";
 
-export default [
-  romantisme,
-  acceptation,
-  acid,
-  vesteAcid,
-  aquarelle,
-  {
-    category: "STYLE",
-    title: "IMPRIMÉ VOLANTÉ",
-    date: "22.04.2019",
-    picture: "/images/style/tKooples.png",
-  },
-  {
-    category: "STYLE",
-    title: "BOHEME",
-    date: "17.10.2018",
-    picture: "/images/style/photoInsta.jpg",
-  },
-];
+export default [romantisme, acceptation, acid, vesteAcid, aquarelle];
