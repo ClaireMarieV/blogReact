39ecef1af44b640e148b98b95865770afd3c6bfe
Claire Vaney <clairevan.54.cv@gmail.com>
2020-05-29 17:41:22 +0200

    remove font-size

--- components/verticalTitle.js
+++ components/verticalTitle.js
@@ -32,7 +32,6 @@ const VerticalTitle = ({ title }) => (
           font-family: arboria, sans-serif;
           font-weight: 500;
           font-style: normal;
-          font-size: 1.2em;
         }
 
         .vertical-title a {
