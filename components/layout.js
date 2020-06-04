63d60f700b1f016841feabafb5147a9eb8349cef
Claire Vaney <clairevan.54.cv@gmail.com>
2020-06-04 14:38:37 +0200

    fixed problem deployement

--- components/layout.js
+++ components/layout.js
@@ -10,7 +10,6 @@ import React from "react";
 import Header from "./header";
 import Footer from "./footer";
 import Canvas from "./canvas";
-import { Frame } from "framer";
 
 const Layout = ({ children }) => (
   <div className="layout">
