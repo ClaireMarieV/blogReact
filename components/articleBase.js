1bfa9d865fe936c81bc29273b8cc724649abf6de
Claire Vaney <clairevan.54.cv@gmail.com>
2020-07-01 13:11:21 +0200

    added width articleBase

--- components/articleBase.js
+++ components/articleBase.js
@@ -4,30 +4,32 @@ import ArticleBaseTitle from "./articleBaseTitle";
 import Picture from "./picture";
 
 const ArticleBase = ({ title, children, date, className, id }) => (
-  <div className={"article-base" + (className || "")} id={id}>
-    <ArticleBaseTitle title={title} date={date} />
-    <div>{children}</div>
-    <style jsx global>{`
-      .article-base {
-        margin: auto;
-      }
-      .article-base-title {
-        margin: 2rem;
-      }
-      .article-base .article-base-title .article-title h1 {
-        font-family: freight-big-pro, serif;
-        font-weight: 500;
-        font-style: italic;
-        letter-spacing: -0.03rem;
-      }
-      .article-base img {
-        max-width: 50rem;
-        width: 100%;
-        object-fit: cover;
-        margin: 5rem 0 0;
-      }
-    `}</style>
-  </div>
+  <>
+    <div className={"article-base" + (className || "")} id={id}>
+      <ArticleBaseTitle title={title} date={date} />
+      <div>{children}</div>
+      <style jsx global>{`
+        .article-base {
+          margin: auto;
+          max-width: 70rem;
+        }
+        .article-base-title {
+          margin: 2rem;
+        }
+        .article-base .article-base-title .article-title h1 {
+          font-family: freight-big-pro, serif;
+          font-weight: 500;
+          font-style: italic;
+          letter-spacing: -0.03rem;
+        }
+        .article-base img {
+          width: 100%;
+          object-fit: cover;
+          margin: 5rem 0 0;
+        }
+      `}</style>
+    </div>
+  </>
 );
 
 export default ArticleBase;
