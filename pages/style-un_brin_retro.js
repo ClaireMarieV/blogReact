e8271649fbbe8cf23b0b7f2b1f1d9d0018462a1b
Claire Vaney <clairevan.54.cv@gmail.com>
2020-07-10 21:26:49 +0200

    re-import component

--- pages/style-un_brin_retro.js
+++ pages/style-un_brin_retro.js
@@ -4,6 +4,7 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 import ArticleBase from "../components/articleBase";
+import ArticleBaseTitle from "../components/articleBaseTitle";
 import article from "../data/style/vesteAcid";
 
 const RetroPage = () => (
