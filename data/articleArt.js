264f04c8e652b0e74c552e8e26351d24f2eef5d5
Claire Vaney <clairevan.54.cv@gmail.com>
2020-07-22 12:37:12 +0200

    added resizing picture

--- data/articleArt.js
+++ data/articleArt.js
@@ -1,56 +1,75 @@
+import naturePreview from "../public/images/art/nature.jpg?lqip";
+import nature from "../public/images/art/nature.jpg?resize&sizes[]=300&sizes[]=600&sizes[]=1000";
+import envolePreview from "../public/images/art/envoleGrue.jpg?lqip";
+import envoleGrue from "../public/images/art/envoleGrue.jpg?resize&sizes[]=300&sizes[]=600&sizes[]=1000";
+import equilibrePreview from "../public/images/art/equilibre.jpg?lqip";
+import equilibre from "../public/images/art/equilibre.jpg?resize&sizes[]=300&sizes[]=600&sizes[]=1000";
+import energiePreview from "../public/images/art/energie.jpg?lqip";
+import energie from "../public/images/art/energie.jpg?resize&sizes[]=300&sizes[]=600&sizes[]=1000";
+import reveriePreview from "../public/images/art/reverie.jpg?lqip";
+import reverie from "../public/images/art/reverie.jpg?resize&sizes[]=300&sizes[]=600&sizes[]=1000";
+import venusPreview from "../public/images/art/venus.jpg?lqip";
+import venus from "../public/images/art/venus.jpg?resize&sizes[]=300&sizes[]=600&sizes[]=1000";
+import racinePreview from "../public/images/art/racine.jpg?lqip";
+import racine from "../public/images/art/racine.jpg?resize&sizes[]=300&sizes[]=600&sizes[]=1000";
+import cercleViePreview from "../public/images/art/cercleVie.jpg?lqip";
+import cercleVie from "../public/images/art/cercleVie.jpg?resize&sizes[]=300&sizes[]=600&sizes[]=1000";
+import gruePreview from "../public/images/art/grue.jpg?lqip";
+import grue from "../public/images/art/grue.jpg?resize&sizes[]=300&sizes[]=600&sizes[]=1000";
+
 export default [
   {
     category: "art",
     title: "NATURE DE L'ÉMERVEILLEMENT",
     date: "06.04.2019",
-    picture: "/images/art/nature.jpg",
+    picture: { preview: naturePreview, picture: nature },
   },
   {
     category: "art",
     title: "RENAISSANCE DU PHOENIX",
     date: "06.04.2019",
-    picture: "/images/art/envoleGrue.jpg",
+    picture: { preview: envolePreview, picture: envoleGrue },
   },
   {
     category: "art",
     title: "EQUILIBRE",
     date: "23;04.2020",
-    picture: "/images/art/equilibre.jpg",
+    picture: { preview: equilibrePreview, picture: equilibre },
   },
   {
     category: "art",
     title: "ESSENCE",
     date: "06.04.2019",
-    picture: "/images/art/energie.jpg",
+    picture: { preview: energiePreview, picture: energie },
   },
   {
     category: "art",
     title: "CO VIDE 1",
     date: "29.03.2020",
-    picture: "/images/art/reverie.jpg",
+    picture: { preview: reveriePreview, picture: reverie },
   },
   {
     category: "art",
     title: "ARCHANGE FLORAL",
     date: "01.03.2020",
-    picture: "/images/art/venus.jpg",
+    picture: { preview: venusPreview, picture: venus },
   },
   {
     category: "art",
     title: "RACINE",
     date: "06.04.2019",
-    picture: "/images/art/racine.jpg",
+    picture: { preview: racinePreview, picture: racine },
   },
   {
     category: "art",
     title: "NATURES HUMANISÉES",
     date: "06.04.2019",
-    picture: "/images/art/cercleVie.jpg",
+    picture: { preview: cercleViePreview, picture: cercleVie },
   },
   {
     category: "art",
     title: "GRUE",
     date: "29.03.2020",
-    picture: "/images/art/grue.jpg",
+    picture: { preview: gruePreview, picture: grue },
   },
 ];
