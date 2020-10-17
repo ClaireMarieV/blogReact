bb042d5f46dcad8ee19c238ab9dbc7a8c8cb091c
Claire Vaney <clairevan.54.cv@gmail.com>
2020-10-17 15:30:22 +0200

    deleted articleTitle

--- components/articleBase.js
+++ components/articleBase.js
@@ -1,17 +1,18 @@
 import React from "react";
 
-import ArticleBaseTitle from "./articleBaseTitle";
 import Picture from "./picture";
 
-const ArticleBase = ({ title, children, date, className, id }) => (
+const ArticleBase = ({ children, className, id }) => (
   <>
     <div className={"article-base" + (className || "")} id={id}>
-      <ArticleBaseTitle title={title} date={date} />
       <div>{children}</div>
+
       <style jsx global>{`
         .article-base {
-          margin: 7rem auto;
-          max-width: 70rem;
+          display: grid;
+          grid-template-columns: repeat(2, minmax(0, 1fr));
+          margin: auto;
+          width: 80vw;
         }
         .article-base img {
           width: 100%;
