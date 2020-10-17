d41a6966166c96a8f9170974e15059dfa9842ec6
Claire Vaney <clairevan.54.cv@gmail.com>
2020-10-17 18:19:39 +0200

    added vertical title to category articles

--- components/verticalIndex.js
+++ components/verticalIndex.js
@@ -1,81 +1,57 @@
 import React from "react";
 import Picture from "./picture";
+
 import VerticalTitle from "./verticalTitle";
 import Link from "next/link";
 import { fromArticle } from "../lib/link";
+
 const VerticalIndex = ({ category, title, date, picture, className, id }) => (
   <div className={"vertical-index " + (className || "")} id={id}>
     <div className="content">
       <Link href={fromArticle({ category, title })}>
         <a>
           <Picture picture={picture} />
+          <VerticalTitle category={category} title={title} date={date} />
         </a>
       </Link>
-      <VerticalTitle title={title} />
     </div>
     <style jsx>{`
-      .vertical-index {
-        display: flex;
-        text-decoration: none;
-        max-width: 33.3vw;
-        max-height: 88.75vh;
-        transform: translate3d(0px, 0px, 0px);
-        height: 100%;
-        margin: 3rem;
-      }
-      .vertical-index:nth-child(odd) {
-        transform: translate3d(-15.5vw, 0, 0);
-      }
-      /****Content*****/
-      .vertical-index .content {
-        overflow: hidden;
-        margin: 0.5rem;
-      }
-      .vertical-index .content a {
-        transition: all 0.2s ease-in;
-      }
-      .vertical-index .content a:hover {
-        padding: 1.5em;
-      }
-      .content .vertical-title {
-        display: flex;
-      }
-      .vertical-index a {
-        display: flex;
-        overflow: hidden;
-        text-decoration: none;
-        height: 100%;
-        transform: scaleX(1);
-        cursor: pointer;
-      }
-      .vertical-index a::before {
-        content: "";
-        z-index: -1;
-        background: #121212;
-        position: absolute;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: 100%;
-        visibility: hidden;
-      }
-      @media (max-width: 720px) {
         .vertical-index {
-          min-width: 100% !important;
           display: flex;
-          margin: 2.5rem;
+          overflow: hidden;
+          text-decoration: none;
+          transform: scaleX(1);
+          cursor: pointer;
+          max-width: 43vw;
+          max-height: 48.75vw;
+          transform: translate3d(0px, 0px, 0px);
+          height: 100%;
+          margin: 3rem;
         }
-      }
-      @media (max-width: 750px) {
         .vertical-index:nth-child(odd) {
-          transform: translate3d(0, 0, 0);
+          transform: translate3d(-15.5vw, 0, 0);
+          animation-timing-function: cubic-bezier(0.1, -0.6, 0.2, 0);
         }
-        .vertical-index .content {
-          padding: 1.3rem;
-          margin: -0.5rem;
+        .vertical-index .content{
+          height: 100%
+          transition: transform .5s cubic-bezier(.455,.03,.515,.955);
+          transition: transform .5s cubic-bezier(.455,.03,.515,.955);
+          position: relative;
+          width: 100%;
         }
-      }
-    `}</style>
+        .vertical-index a  {
+          display: flex;
+          overflow: hidden;
+          text-decoration: none;
+          transform: scaleX(1);
+          cursor: pointer;
+          transition: transform 0.5s cubic-bezier(0.455, 0.03, 0.515, 0.955);
+          cursor: pointer;
+          height: 100%;
+        }
+    
+      `}</style>
   </div>
 );
+
 export default VerticalIndex;
--- components/verticalTitle.js
+++ components/verticalTitle.js
@@ -2,29 +2,62 @@ import React from "react";
 
 const VerticalTitle = ({ title }) => (
   <div className="vertical-title">
-    <h3>{title}</h3>
+    <div className="content">
+      <h3>{title}</h3>
+    </div>
     <style jsx>
       {`
-        @media (max-width: 500px) {
-          .vertical-title {
-            line-height: normal;
-            right: -6.5vw;
-          }
+        .vertical-title {
+          text-align: right;
+          writing-mode: vertical-rl;
+          transform: rotate(180deg);
+          display: flex;
+          height: 100%;
+          line-height: 1.875vw;
+          margin: 0 0 1em;
         }
-        @media (min-width: 400px) and (max-width: 730px) {
-          .vertical-title h3 {
-            font-size: 1rem;
-          }
+        .vertical-title .content {
+          padding-top: 1rem;
         }
-        @media (min-width: 307px) and (max-width: 399px) {
-          .vertical-title h3 {
-            font-size: 0.8rem;
-          }
+
+        .vertical-title h3 {
+          text-align: center;
+          letter-spacing: 0.5rem;
+          font-family: astoria-sans-condensed, sans-serif;
+          font-weight: 500;
+          font-style: normal;
+          font-size: 1.2em;
         }
-        @media (max-width: 306px) {
-          .vertical-title h3 {
-            font-size: 0.7rem;
-          }
+        .vertical-title .content a {
+          text-decoration: none;
+          padding: 0 0 0.35em;
+          line-height: 1.4;
+          transition: all 0.5s;
+          transition-timing-function: cubic-bezier(0.2, 1, 0.3, 1);
+        }
+
+        .vertical-title .content a::before {
+          content: "";
+          position: absolute;
+          z-index: -1;
+          width: 4rem;
+          height: 1rem;
+          opacity: 0.3;
+          transform: scale3d(0, 1, 1);
+          transform-origin: 0% 50%;
+          transition: transform 0.5s;
+          transition-timing-function: cubic-bezier(0.2, 1, 0.3, 1);
+        }
+        .vertical-title .content a:hover,
+        .vertical-title .content a:focus {
+          color: #563440;
+        }
+        .vertical-title .content a::before {
+          background: #563440;
+        }
+        .vertical-title:hover .content a::before,
+        .vertical-title:focus .content a::before {
+          transform: scale3d(1, 1, 1);
         }
       `}
     </style>
