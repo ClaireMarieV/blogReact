b52c46256d1bec7312e7a018f5d2158f021adbe9
Claire Vaney <clairevan.54.cv@gmail.com>
2020-10-18 16:30:59 +0200

    deleted article basique

--- data/articleShoes.js
+++ data/articleShoes.js
@@ -1,7 +1,6 @@
 import desir from "./shoes/desir";
 import plateforme from "./shoes/plateforme";
 import mango from "./shoes/mangoBottega";
-import basique from "./shoes/basique";
 import neutrals from "./shoes/neutrals";
 
 export default [neutrals, mango, plateforme, desir];
