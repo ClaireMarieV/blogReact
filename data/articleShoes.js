5d99051746ead734841c45aefec11870c8ebcaf4
Claire Vaney <clairevan.54.cv@gmail.com>
2020-10-22 18:38:29 +0200

    put aside article neutrals and romantisme

--- data/articleShoes.js
+++ data/articleShoes.js
@@ -1,6 +1,6 @@
 import desir from "./shoes/desir";
 import plateforme from "./shoes/plateforme";
 import mango from "./shoes/mangoBottega";
-import neutrals from "./shoes/neutrals";
+// import neutrals from "./shoes/neutrals";
 
-export default [neutrals, mango, plateforme, desir];
+export default [mango, plateforme, desir];
--- data/articleStyle.js
+++ data/articleStyle.js
@@ -1,8 +1,8 @@
 import acceptation from "./style/acceptation";
 import acid from "./style/acid";
 import vesteAcid from "./style/vesteAcid";
-import romantisme from "./style/romantisme";
+// import romantisme from "./style/romantisme";
 import rustique from "./style/rustique";
 import grunge from "./style/simpleGrunge";
 
-export default [grunge, rustique, acceptation, romantisme, acid];
+export default [grunge, rustique, acceptation, acid];
