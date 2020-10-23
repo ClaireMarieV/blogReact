1a64986e67f4b8980c520047b71ff648e20080e4
Claire Vaney <clairevan.54.cv@gmail.com>
2020-10-23 14:50:17 +0200

    modified text article

--- data/style/acceptation.js
+++ data/style/acceptation.js
@@ -23,22 +23,20 @@ export default {
           <br />A vrai dire j'ai mis du temps ... Pour ce début d'acceptation à
           bientôt 32 ans, je suis passée par beaucoup de petites étapes. A 19ans
           je pesais 75kilos, 'le déclic' s'est fait à la suite d'un cadeau
-          d'anniversaire que ma belle-mère et mon père m'avaient fait. Vous
-          voulez savoir ce que c'était? Attention, on va entrer dans un
-          stéréotype : Une balance. Ils pensaient que c'était un cadeau utile
-          comme je n'en avais pas mais ça a aussi engendré des complexes par la
-          suite. J'ai perdu 27kilos en 3ans après m'être pesée. Régimes à
-          répétition, chacun suivi d'une période de stabilisation et de sports
-          (heureusement!), mais j'ai aussi commencé à fumer à 19ans pour brûler
-          des calories et perdre du poids plus vite... Je ne m'acceptais pas à
-          tel point que, comme beaucoup d'autres jeunes femmes, je suis entrée
-          au début dans un troubles alimentaire. Certaines se demanderont
-          comment est-ce que l'on peut infliger ça à son corps dans le seul but
-          de perdre du poids, prôneront le bien-être. Mais c'est vrai, comment?
-          Et bien ce n'est que depuis peu que l'ont met en avant tous les corps.
-          Même s'il y a eu ce phénomène marketing autour et donc heureusement
-          une prise de conscience, je voyais bien le regard qu'avaient les
-          hommes sur moi quand j'étais en surpoids. Aucun.
+          d'anniversaire que ma belle-mère et mon père m'avaient fait: une
+          balance. Ils pensaient que c'était un cadeau utile comme je n'en avais
+          pas mais ça a aussi engendré des complexes par la suite. J'ai perdu
+          27kilos en 3/4ans après m'être pesée. Régimes à répétition, chacun
+          suivi d'une période de stabilisation et de sports (heureusement!),
+          mais j'ai aussi commencé à fumer à 19ans pour brûler des calories et
+          perdre du poids plus vite... Je ne m'acceptais pas à tel point que,
+          comme beaucoup d'autres jeunes femmes, je suis entrée au début dans
+          des troubles alimentaire. Certaines se demanderont comment est-ce que
+          l'on peut infliger ça à son corps dans le seul but de perdre du poids,
+          prôneront le bien-être. Mais c'est vrai, comment? Et bien ce n'est que
+          depuis peu que l'ont met en avant tous les corps. Même s'il y a eu ce
+          phénomène marketing autour et donc heureusement une prise de
+          conscience, à ce moment le regard sur mon corps était dur.
         </p>
       </Framer>
       <Framer>
@@ -59,10 +57,10 @@ export default {
           accompagné pendant quelque pas et m'a dit que ça ne serait pas
           possible. Pas de problème, je demande à ma soeur de venir me chercher.
           Mais après ça, ma soeur et son copain se sont énervés. Ils ont essayé
-          de le contacter tous les jours de la semaine sans réponses, ma soeur
-          est allé devant chez lui, sonner, taper à la porte, crier contre lui,
-          a essayé de l'appeler sans réponse. De ce que je me souviens, il a
-          fait le mort pendant des années ensuite.
+          de le contacter pendant plusieur jours sans réponses, Ils sont allés
+          devant chez lui, sonner, taper à la porte, crier, ont essayé de
+          l'appeler sans réponse. De ce que je me souviens, il n'a plus donné
+          signe de vie pendant plusieurs mois.
         </p>
       </Framer>
       <Framer>
@@ -70,7 +68,7 @@ export default {
       </Framer>
       <Framer>
         <p>
-          Pas de problème? Si! Evidemment! C'est un grand manque de respect!
+          Pas de problème? Si, évidemment. C'est un grand manque de respect!
           Cette première experience a mis à l'épreuve l'estime de moi et celle
           que je pouvais avoir en les autres. C'est pourquoi à partir de ce
           moment, j'ai décidé de perdre encore plus de poids. Une espèce de
@@ -86,26 +84,23 @@ export default {
           rencontres, j'avais toujours ce système de mit en place. Cette
           vengeance après chaque rupture qui ne blessait personne, de 56 à 50 de
           50 jusqu'a me stabiliser une bonne fois à 48. Il y a 3ans et demi à
-          peu près, je me suis coupée de personnes qui étaient toxiques. Un
-          groupe qui s'étaient trouvés parce que sans doute ils avaient en
-          commun le fait de ne pas se sentir bien.
+          peu près, je me suis coupée de personnes qui étaient toxiques.
         </p>
       </Framer>
       <Framer>
         <p>
           Depuis cette rupture avec eux, tout a changé. J'ai pu me recentrer,
           chercher à aller mieux et c'est quelques mois plus tard que j'ai
-          rencontré quelqu'un. Ça fait 2ans et 4mois que l'on est ensemble (et
-          oui je compte encore les mois haha). Aujourd'hui ça va faire un mois
-          que j'ai arrêté de fumer; c'est pourquoi je fais cet article
-          d'ailleurs. Un mois où je réussi à refaire du sport, des exercices à
-          la barre, du yoga. Un mois où je retrouve une certaine féminité. J'ai
-          même redansé, chose qui ne m'étais pas arrivé depuis plusieurs mois.
-          Ces dernières années, je n'ai pas été douce avec mon corps, ni avec
-          mon esprit d'ailleurs. J'avais l'impression de devoir me prouver que
-          dans ce monde une femme peut être plus forte qu'un homme. Je ne vous
-          ai donné que peu d'exemples du pourquoi mais le principal n'est pas
-          là.
+          rencontré quelqu'un. Ça fait maintenant près de 3ans que l'on est
+          ensemble. Aujourd'hui ça va faire un mois que j'ai arrêté de fumer;
+          c'est pourquoi je fais cet article d'ailleurs. Un mois où je réussi à
+          refaire du sport, des exercices à la barre, du yoga. Un mois où je
+          retrouve une certaine féminité. J'ai même redansé, chose qui ne
+          m'étais pas arrivé depuis plusieurs mois. Ces dernières années, je
+          n'ai pas été douce avec mon corps, ni avec mon esprit d'ailleurs.
+          J'avais l'impression de devoir me prouver que dans ce monde une femme
+          peut être plus forte qu'un homme. Je ne vous ai donné que peu
+          d'exemples du pourquoi mais le principal n'est pas là.
         </p>
       </Framer>
       <Framer>
@@ -119,7 +114,7 @@ export default {
           vestimentaire. La douceur du tissus et l'élégance qui s'en dégage. La
           démarche que l'on a en talons ou encore la légereté et le confort que
           l'on peut ressentir en basket. Toutes ces petites choses que l'on ne
-          prend plus forcément le temps d'apprécier ni même de se rendre compte.
+          prend plus forcément le temps d'apprécier.
           <br />
           C'est pourquoi, maintenant, ce que je recherche c'est ça. Etre en
           équilibre entre les racines et le ciel. C'est pourquoi la danse et le
