c4cb821cde3d3d5608498e95403f9d8e966e3d2a
Claire Vaney <clairevan.54.cv@gmail.com>
2020-10-24 21:25:44 +0200

    modified resizing picture

--- data/style/simpleGrunge.js
+++ data/style/simpleGrunge.js
@@ -1,11 +1,11 @@
 import Picture from "../../components/picture";
 import Framer from "../../components/framer";
 import grungePreview from "../../public/images/style/simpleGrunge1.png?lqip";
-import grunge from "../../public/images/style/simpleGrunge1.png?resize&sizes[]=300&sizes[]=600&sizes[]=1000";
+import grunge from "../../public/images/style/simpleGrunge1.png?resize&sizes[]=300&sizes[]= 650";
 import grunge2Preview from "../../public/images/style/simpleGrunge2.png?lqip";
-import grunge2 from "../../public/images/style/simpleGrunge2.png?resize&sizes[]=300&sizes[]=600&sizes[]=1000";
+import grunge2 from "../../public/images/style/simpleGrunge2.png?resize&sizes[]=300&sizes[]= 650";
 import grunge3Preview from "../../public/images/style/simpleGrunge3.png?lqip";
-import grunge3 from "../../public/images/style/simpleGrunge3.png?resize&sizes[]=300&sizes[]=600&sizes[]=1000";
+import grunge3 from "../../public/images/style/simpleGrunge3.png?resize&sizes[]=300&sizes[]= 650";
 export default {
   category: "STYLE",
   title: "SIMPLE GRUNGE",
@@ -14,19 +14,28 @@ export default {
   content: (
     <>
       <Framer>
-        <Picture picture={{ picture: grunge }} />
+        <Picture
+          picture={{ picture: grunge }}
+          alt="illustration+robe fendue grunge"
+        />
       </Framer>
       <Framer>
         <p></p>
       </Framer>
       <Framer>
-        <Picture picture={{ picture: grunge2 }} />
+        <Picture
+          picture={{ picture: grunge2 }}
+          alt="illustration+robe fendue grunge"
+        />
       </Framer>
       <Framer>
         <p></p>
       </Framer>
       <Framer>
-        <Picture picture={{ picture: grunge3 }} />
+        <Picture
+          picture={{ picture: grunge3 }}
+          alt="illustration+tenue avec manteau"
+        />
       </Framer>
       <Framer>
         <p></p>
