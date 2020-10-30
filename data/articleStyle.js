9bd333e6a8e00f8c4a04e9b1291d980e719c7506
Claire Vaney <clairevan.54.cv@gmail.com>
2020-10-30 16:21:32 +0100

    added article acceptation to comments

--- data/articleStyle.js
+++ data/articleStyle.js
@@ -1,7 +1,7 @@
-import acceptation from "./style/acceptation";
+// import acceptation from "./style/acceptation";
 import acid from "./style/acid";
 // import romantisme from "./style/romantisme";
 import rustique from "./style/rustique";
 import grunge from "./style/simpleGrunge";
 
-export default [grunge, rustique, acceptation, acid];
+export default [grunge, rustique, acid];
